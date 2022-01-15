b4dfa43c4531e711004e303e9ad6e6cc7f36da4e
timre13 <itorteli13@gmail.com>
2022-01-15 18:22:49 +0100

    scripts: Download more themes

--- scripts/dl_themes.py
+++ scripts/dl_themes.py
@@ -17,6 +17,13 @@ if not os.path.exists(SAVE_PATH):
 print("Fetching list... ", end="", flush=True)
 list = [[it.find("a", href=True).text, it.find("a", href=True)["href"].split("=")[2]] for it in BeautifulSoup(urlopen(LIST_URL), features="html.parser").find("div", class_="listing").find_all("div", class_="theme")] #type:ignore
 print("Found {} themes".format(len(list)))
+# Add some more nice themes
+list.extend([
+    ["Atom One Dark",   "46248"],
+    ["Darkula",         "15515"],
+    ["Gruvbox Dark",    "23468"],
+    ["Molokai",         "3908" ],
+])
 
 for i, it in enumerate(list):
     print("[{}/{}] Downloading: {} (id={})... ".format(str(i+1).rjust(2, ' '), len(list), it[0], it[1]), end="", flush=True)
